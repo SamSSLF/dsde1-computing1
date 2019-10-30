50eacbf3dbe61a8fc460495cf5538226f39c0acc
SamSSLF <56586268+SamSSLF@users.noreply.github.com>
2019-10-30 16:20:29 +0000

    trying isinstance()

--- Week-3/pendulum.py
+++ Week-3/pendulum.py
@@ -1,5 +1,17 @@
 import math
 
 def period(L,g):
+    
+    x = isinstance(L,(float,int))
+    if x == False:
+        print('Wrong data type for L, please enter a number and try again.')
+    
+    y = isinstance(g,(float,int))
+    if y == False:
+        print('Wrong data type for g, please enter a number and try again.')
+    
     T = 2*3.14*math.sqrt(L/g)
+    
     return T
+
+period(5,9.81)
